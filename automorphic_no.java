a144900c9ba2e0bdd0d6a8e7e5411729c838552e
Namanjha-777 <143312876+Namanjha-777@users.noreply.github.com>
2023-08-27 15:04:06 +0530

    Add files via upload

---
+++ automorphic_no.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class automorphic_no{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the no");
+        int n = sc.nextInt();
+        int b =n*n;
+        if(b%10==n%10){
+            System.out.println("no is automorphic");
+        }else{
+            System.out.println("no is automorphic");
+        }
+    }
+}
+        
